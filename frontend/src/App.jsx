e309fca1734994227c2831a889ff6200da3d2fca
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-07-02 17:39:30 +0800

    Header applied

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Header from './assets/Header.jsx';
 import UserLogin from './user/UserLogin.jsx';
 import UserDashboard from './user/UserDashboard.jsx';
 import Room from './user/reservation/RoomReservation.jsx';
@@ -10,9 +11,23 @@ function App() {
     <Router>
       <div>
         <Routes>
-          <Route path="/user" element={<UserLogin />} />
-          <Route path="/user/dashboard" element={<UserDashboard />} />
-          <Route path="/user/reserve" element={<Room />} />
+        <Route path="/user" element={
+            <>
+            <UserLogin />
+            </>
+            } />
+          <Route path="/user/dashboard" element={
+            <>
+            <Header/>
+            <UserDashboard />
+            </>            
+            } />
+          <Route path="/user/reserve" element={
+            <>
+            <Header/>
+            <Room />
+            </>
+            } />
         </Routes>
       </div>
     </Router>
