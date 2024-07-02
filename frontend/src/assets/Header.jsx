e682052751e7044b775e502e4080aa86eda2d7b5
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-07-02 17:08:49 +0800

    header page

---
+++ frontend/src/assets/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+  return (
+    <nav className="navbar navbar-expand-md py-3">
+      <div className="container-fluid">
+      </div>
+    </nav>
+  );
+};
+
+export default Header;
