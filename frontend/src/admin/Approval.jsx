5e10b2b599cbc4fcdb721908c98090b1e17c9dd2
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-07-10 18:00:21 +0800

    gret

--- frontend/src/admin/Approval.jsx
+++ frontend/src/admin/Approval.jsx
@@ -1,42 +1,73 @@
 import {React, useState} from 'react';
 
 const Approval =()=>{
+
+    const [rejectModal, rejectModalState] = useState(false);
+    const [acceptModal, acceptModalState] = useState(false);
+
+    const cancelReject =()=>{
+        rejectModalState(false);
+    }
+    const cancelApprove =()=>{
+        acceptModalState(false);
+    }
+    const handleApprove =()=>{
+        acceptModalState(true);
+    }
+    const handleReject =()=>{
+        rejectModalState(true);
+    }
     return(
         <div className='listCont1'>
             <h1>For Approval</h1>
+            <h2 style={{textAlign: 'center'}}>PALAWAN ROOM</h2>
             <div className='approvalGroup'>
                 <div className='approvalMeets'>
                     <div className='approvalDeets'>
-                    <h2>Palawan Room</h2>
-                    <p>Meeting Title: Meeting With Client</p>
+                    <h2>Meeting With Client</h2>
                     <p>Date and Time: 09:00 A.M.-10:00 A.M.</p>
                     <p>Reserved By: Jona K.</p>
                     <p> Members: Employee 1, Employee 2 </p>
                     <hr/>
                     <div className='approvalGrp'>
-                        <button type='reject' >Reject</button>
-                        <button type = 'approve'>Approve</button>
+                        <button type='not-appr' onClick={handleReject}>Reject</button>
+                        <button type = 'appr' onClick ={handleApprove}>Approve</button>
                     </div>
                     </div>
 
                 </div>
                 <div className='approvalMeets'>
                     <div className='approvalDeets'>
-                    <h2>Palawan Room</h2>
-                    <p>Meeting Title: Meeting With Client</p>
+                    <h2>Meeting With Client</h2>
                     <p>Date and Time: 09:00 A.M.-10:00 A.M.</p>
                     <p>Reserved By: Jona K.</p>
                     <p>Members: Employee 1, Employee 2 </p>
+                    <hr/>
                         <div className='approvalGrp'>
-                            <button type='reject' >Reject</button>
-                            <button type = 'approve'>Approve</button>
+                            <button type='not-appr'onClick={handleReject} >Reject</button>
+                            <button type = 'appr'onClick ={handleApprove}> Approve</button>
                         </div>
                     </div>
-
+                    {rejectModal && (
+                        <div className='gen_modal'>
+                            <div className='gen_modal-content'>
+                                <p>Are you sure you want to reject the meeting?</p>
+                                <button type='cancel' onClick={cancelReject}>Cancel</button>
+                                <button type= 'reject'>Yes, reject</button>
+                            </div>
+                        </div>
+                    )}
+                    {acceptModal &&(
+                        <div className='gen_modal'>
+                            <div className='gen_modal-content'>
+                                <p>Are you sure you want to approve the meeting?</p>
+                                <button type='cancel' onClick={cancelApprove}>Cancel</button>
+                                <button type='approve'>Yes, approve</button>
+                                </div>
+                        </div>
+                    )}
                 </div>
-                <div className='approvalMeets'>
 
-                </div>
             </div>
         </div>
     );
--- frontend/src/admin/ApprovalRooms.jsx
+++ frontend/src/admin/ApprovalRooms.jsx
@@ -1,18 +1,24 @@
 import React from 'react';
 import roomBg from '../assets/roombg.jpg';
+import { useNavigate } from 'react-router-dom';
 
 const ApprovalRoom=()=>{
-    const Rooms = ["Palawan", "Boracay", "Palawan and Boracay"]
+    const navigate = useNavigate();
+    const Rooms = ["Palawan", "Boracay", "Palawan and Boracay"];
+
+    const approvalHandling =()=>{
+        navigate('/admin/approval')
+    }
     return(
         <div className='listCont3' >
-        
-        <div className="card-container">
+        <h1> For Approval - Rooms</h1>
+        <div className="approval-card-container">
           {Rooms.map((place, index) => (
-            <div key={`Rooms-${index}`} className="card" style={{ backgroundImage: `url(${roomBg})` }}>
-              <div className="overlay">
-                <h3>{place}</h3>
-                <button className="reserve-btn">
-                  Reserve
+            <div key={`Rooms-${index}`} className="approveRooms" style={{ backgroundImage: `url(${roomBg})` }}>
+              <div className="overlayy">
+                <h1>{place}</h1>
+                <button className="forApproval-btn" onClick={approvalHandling}>
+                  See Pending Approvals
                 </button>
               </div>
             </div>
