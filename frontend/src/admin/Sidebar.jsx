96bca47dd8c2b6afc66f0ec31ffb3f2e17dd7e1c
2916lalala <74847517+2916lalala@users.noreply.github.com>
2024-07-11 11:20:11 +0800

    fd

--- frontend/src/admin/Sidebar.jsx
+++ frontend/src/admin/Sidebar.jsx
@@ -1,68 +1,91 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
-import { FaUserCircle, FaBell, FaCalendarCheck } from 'react-icons/fa';
-import { MdGroups } from "react-icons/md";
+import * as FaIcons from 'react-icons/fa';
+import * as MdIcons from "react-icons/md";
 import { HiUserAdd } from 'react-icons/hi';
 import logo from '../assets/logos/GDSLogo.png';
 import profile from '../assets/Default Avatar.png';
 import './AdminPages.css';
 
 const SidebarComponent = () => {
+    const [sidebar, setSidebar] = useState(false);
     const userName = "Juan D.C.";
     const department = "Starlight";
 
-    return (  
-        <nav className="navbar">
-            <div className="sidebar-logo">
-                <img src={logo} alt="Logo" className="logo-img" />
-            </div>
-            <div className='profileDetail'>
-                <div className="profileSide">
-                    <img src={profile} alt="profile" />
-                    <div>
-                        <h2 style={{margin:'0'}}>{userName}</h2>
-                        <p>{department}</p>
-                    </div>
+    const showSidebar =()=>{
+        setSidebar(true);
+    }
+
+    const hideSidebar =()=>{
+        setSidebar(false);
+    }
+
+    return (
+        <>
+        {/* state icons */}
+        <div className='nav-icon'>
+            <Link to='#' className='menu-logo'>
+                <FaIcons.FaBars onClick={showSidebar}/>
+            </Link>
+        </div>
+        
+        <nav className={sidebar ? 'menu active' : 'menu'} >
+        <ul>
+        <div className='nav-icon'>
+            <Link to='#' className='menu-logo'>
+                <MdIcons.MdClose  onClick={hideSidebar}/>
+            </Link>
+        </div>
+        <div className="sidebar-logo">
+            <img src={logo} alt="Logo" className="logo-img" />
+        </div>
+        <div className='profileDetail'>
+            <div className="profileSide">
+                <img src={profile} alt="profile" />
+                <div>
+                    <h2 style={{margin:'0'}}>{userName}</h2>
+                    <p>{department}</p>
                 </div>
             </div>
+        </div>
+        <li className='profileMenu'>
+            <h1>Profile Menu</h1>
+            <hr />
+            <li>
+                <Link to="/profile-settings">
+                    <FaIcons.FaUserCircle /> Profile Settings
+                </Link>
+            </li>
+            <li>
+                <Link to="/notifications">
+                    <span className="notifCount">-</span>
+                    <FaIcons.FaBell /> Notifications
+                </Link>
+            </li>
+        </li>
+        <li className='adminMenu'>
+            <h1>Admin Menu</h1>
+            <hr />
+            <li>
+                <Link to="/admin/employee-list">
+                    <MdIcons.MdGroups/> Employee List
+                </Link>
+            </li>
+            <li>
+                <Link to="/admin/add-employee">
+                    <HiUserAdd /> Add Employee
+                </Link>
+            </li>
+            <li>
+                <Link to="/admin/approval">
+                    <FaIcons.FaCalendarCheck /> For Approval
+                </Link>
+            </li>
+        </li>
+    </ul>
+</nav>
 
-            <ul className="menu">
-                <li className='profileMenu'>
-                    <h1>Profile Menu</h1>
-                    <hr />
-                    <li>
-                        <Link to="/profile-settings">
-                            <FaUserCircle /> Profile Settings
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/notifications">
-                            <span className="notifCount">5</span>
-                            <FaBell /> Notifications
-                        </Link>
-                    </li>
-                </li>
-                <li className='adminMenu'>
-                    <h1>Admin Menu</h1>
-                    <hr />
-                    <li>
-                        <Link to="/admin/employee-list">
-                        <MdGroups/> Employee List
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/add-employee">
-                            <HiUserAdd /> Add Employee
-                        </Link>
-                    </li>
-                    <li>
-                        <Link to="/admin/approval">
-                            <FaCalendarCheck /> For Approval
-                        </Link>
-                    </li>
-                </li>
-            </ul>
-        </nav>
+        </>  
     );
 };
 
