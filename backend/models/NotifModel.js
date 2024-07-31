0cdf1f57942879e64760ae200496c2490879360e
James De Sena <jamesdesena27@gmail.com>
2024-07-31 10:49:00 +0800

    , ,,

--- backend/models/NotifModel.js
+++ backend/models/NotifModel.js
@@ -33,6 +33,10 @@ const NotifSchema = new Schema({
     type: Boolean,
     default: false
   },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 module.exports = mongoose.model("notif", NotifSchema);
