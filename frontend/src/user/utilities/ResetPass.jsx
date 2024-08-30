aa9aa73112bb6978edbbb0a266f10e15ad810a5c
James De Sena <jamesdesena27@gmail.com>
2024-08-30 13:17:39 +0800

    Update ResetPass.jsx

--- frontend/src/user/utilities/ResetPass.jsx
+++ frontend/src/user/utilities/ResetPass.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useLocation, useParams } from 'react-router-dom';
+import { useLocation, useParams, useLocation } from 'react-router-dom';
 import NotFoundAuth from '../../auth/NotFoundAuthReset';
 import axios from 'axios';
 import { ToastContainer, toast } from 'react-toastify';
